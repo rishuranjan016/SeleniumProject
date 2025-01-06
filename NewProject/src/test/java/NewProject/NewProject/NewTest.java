fbeab2f47a2cdac3652d2b3e8146d544de874199
My PC <My PC@DESKTOP-B0VD79F>
2025-01-06 20:34:11 +0530

    22

--- NewProject/src/test/java/NewProject/NewProject/NewTest.java
+++ NewProject/src/test/java/NewProject/NewProject/NewTest.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void f() {
-	  System.out.println("Hello World Again2");
+	  System.out.println("Hello World Again22");
   }
 }
