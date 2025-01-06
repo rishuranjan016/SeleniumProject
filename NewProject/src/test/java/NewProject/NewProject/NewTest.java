02151a17c12cdee278d6d1ccf305cdfd85ebda79
My PC <My PC@DESKTOP-B0VD79F>
2025-01-06 18:01:48 +0530

    FirstCommit

---
+++ NewProject/src/test/java/NewProject/NewProject/NewTest.java
@@ -0,0 +1,9 @@
+package NewProject.NewProject;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+}
---
+++ NewProject/src/test/java/NewProject/NewProject/TestClass.java
@@ -0,0 +1,12 @@
+package NewProject.NewProject;
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+	@Test
+	void run()
+	{
+		
+	}
+
+}
